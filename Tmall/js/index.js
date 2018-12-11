746faf8d476ea3999f3931cde6aaa373e7add8cc
WXG9722 <1321844625@qq.com>
2018-12-11 21:40:36 +0800

    study

--- Tmall/js/index.js
+++ Tmall/js/index.js
@@ -1,17 +1,45 @@
-// 选项卡
-var oHeadDiv = document.getElementById('floor-tab-head');
-var aLi = oHeadDiv.getElementsByTagName('li');
-var oBodyDiv = document.getElementById('floor-tab-body');
-var aUl = oBodyDiv.getElementsByTagName('ul');
+function carouselMap(tabID, tabHeadID, tabHeadTagName, tabBodyID, tabBodyTagName, tabHeadGoal, tabBodyGoal){
+    var tab = document.getElementById(tabID)
+    var oHeadDiv = document.getElementById(tabHeadID);
+    var aLi = oHeadDiv.getElementsByTagName(tabHeadTagName);
+    var oBodyDiv = document.getElementById(tabBodyID);
+    var aDiv = oBodyDiv.getElementsByTagName(tabBodyTagName);
+    var iNow = 0;
+    var timer;
 
-for(var i=0; i<aLi.length; i++){
-    aLi[i].index = i;
-    aLi[i].onmouseover = function(){
-        for(var j=0 ;j<aLi.length; j++){
-            aLi[j].className = "";
-            aUl[j].className = "";
+    function change(idx){
+        for(var i=0 ;i<aLi.length; i++){
+            aLi[i].className = "";
+            aDiv[i].className = "";
         }
-        this.className = "selected";
-        aUl[this.index].className = "active";
+        aLi[idx].className = tabHeadGoal;
+        aDiv[idx].className = tabBodyGoal;
     }
+    function run(){
+        timer = setInterval(function(){
+            iNow++;
+            if(iNow >= aLi.length){
+                iNow = 0;
+            }
+            change(iNow);
+        },1000)
+    }
+    for(var i=0; i<aLi.length; i++){
+        aLi[i].index = i;
+        aLi[i].onmouseover = function(){
+            change(this.index);
+            iNow = this.index;
+        }
+    }
+    run();
+    tab.onmouseover = function(){
+        clearInterval(timer);
+    }
+    tab.onmouseout = function(){
+        run();
+    }
+    
 }
+// 执行
+carouselMap('slide-box','slide-btn-box','li','slide-img-box','a','slide-selected','slide-active');
+carouselMap('floor-tab-item','floor-tab-head','li','floor-tab-body','ul','selected','active');
