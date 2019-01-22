427380aba4c20021110fc5aa67a4e9a920a5e04e
WXG9722 <1321844625@qq.com>
2019-01-22 14:40:05 +0800

    study

--- studyES6/Ajax.js
+++ studyES6/Ajax.js
@@ -1,18 +1,31 @@
-var ajax = function(){
-    var url = '';
-    var xhr;
-    if(window.XMLHttpRequest){
-        xhr = new XMLHttpRequest();
-    }else{
-        xhr = new ActiveXObject('Microsoft.XMLHTTP');
-    }
-    xhr.open('GET', url, true);
-    // xhr.open('POST', url ,true);
-    // xhr.sendRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhr.send();
-    xhr.addEventListener('readystatechange', ()=>{
-        if(xhr.readyState == 4 && xhr.status == 200){
-            let obj = JSON.parse(xhr.responseText);
+// 作者：王鑫广
+// 名称：Promise封装Ajax
+// 时间：2019.1.21
+const $ajax = function(type, url){
+    return new Promise((resolve, reject) => {
+        let xhr;
+        if(window.XMLHttpRequest){
+            xhr = new XMLHttpRequest();
+        }else{
+            xhr = new ActiveXObject('Microsoft.XMLHTTP');
         }
-    });
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState == 4){
+                if(xhr.status == 200){
+                    console.log('成功');
+                    resolve(this.responseText);
+                }else{
+                    reject(new Error(this.statusText));
+                }
+            }
+        }
+        if(type == 'GET'){
+            xhr.open('GET', url, true);
+            xhr.send();
+        }else if(type == 'POST'){
+            xhr.open('POST', url, true);
+            xhr.setRequsetHeader('Content-type', 'application/www-xxx-form-urlencoded');
+            xhr.send();
+        } 
+    })
 }
