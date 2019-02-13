3f0f12b59d837d0fa8aa4322cf820aa344baf79d
WXG9722 <1321844625@qq.com>
2019-02-13 14:33:55 +0800

    study

--- vue-cli3/src/store.js
+++ vue-cli3/src/store.js
@@ -4,12 +4,21 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  // 存储值 相当于data
   state: {
-    count: 0
+    count: 0,
+    num: 10
   },
+  // 存储方法 相当于methods
   mutations: {
-    add(state, num){
-      state.count += num;
+    add(state, a){
+      state.count += a;
+    }
+  },
+  // 存储计算属性 相当于computed
+  getters: {
+    sum(state){
+      return state.count + state.num
     }
   },
   actions: {
--- vue-cli3/src/views/Count.vue
+++ vue-cli3/src/views/Count.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
         <h1>count页面</h1>
-        <p>{{$store.state.count}}</p>
-        <button @click="$store.commit('add', 1)">add</button>
+        <!-- <p>{{$store.state.count}}</p> -->
+        <p>{{count}}</p>
+        <p>{{num}}</p>
+        <p>两个数的和：{{sum}}</p>
+        <button @click="add(2)">add</button>
     </div>
 </template>
 
 <script>
+    import {mapState, mapMutations, mapGetters} from 'vuex'
     export default {
-        
+        computed: {
+            ...mapState(['count', 'num']),
+            ...mapGetters(['sum'])
+        },
+        methods: mapMutations(['add'])
     }
 </script>
 
